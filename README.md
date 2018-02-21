67be3acd2c7bae290ab256afd13f663e450dd017
Ramiro Bedoya <ramirobedoya@Ramiros-MacBook-Pro.local>
2018-02-21 00:03:16 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+Proyecto creado para poder ejecutar procesos de larga duración y enviar mensajes de su proceso a la vista. 
+
+Tecnologías usadas:
+    .net core 
+    RabbitMQ
+    Angular.js
+    SignalR
+    Docker
+    
+1. Correr el contenedor de rabbitmq 
+
+    $ docker run -d --hostname my-rabbit --name some-rabbit -p 8080:15672 -p 5672:5672  rabbitmq:3-management
+
+2. En la carpeta recive ejecutar
+    $ dotner run 
+
+    La aplicación de consola comenzará a escuchar la cola del rabbitmq
+
+3. correr el Api del backend e invocar la URL 
+    http://localhost:5000/api/values
+
+
+TODO: Hacer una aplicación en Angular que suba un archivo a S3, 
+TODO: Poner una cola con esa URL del archivo subido, 
+TODO: En el servicio, ejecutar el proceso de descargar la base y enviar mensajes a una cola de proceso. 
+TODO: Enviar esos mensajes por medio de SignalR a la vista.
---
+++ backend/api_long_proces/api_long_proces/Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+
+namespace api_long_proces.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                //Console.WriteLine(" [x] Sent {0}", message);
+                return new string[] { message , "value2" };
+
+            }
+
+            //Console.WriteLine(" Press [enter] to exit.");
+            //Console.ReadLine();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
